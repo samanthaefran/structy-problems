3749055628788a94f7e2b4afb8cfb6aab7f9bf73
Samantha Elaine <85855171+ybsamantha@users.noreply.github.com>
2022-08-05 20:16:16 -0400

    heyprogrammer ;)

---
+++ heyprogrammer.js
@@ -0,0 +1,17 @@
+/* 
+
+Task: 
+
+Write a function greet that takes in a string argument, s, and returns the string "hey s".
+
+*/
+
+// my solution 
+
+const greet = (s) => {
+  return(`hey ${s}`)
+ };
+ 
+ module.exports = {
+   greet,
+ };
